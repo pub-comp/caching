2a7ce2d00932176de9055789f7feed0d35a8ad1c
doug144_cp <96feba1dc3e5b2bffbf51bf1a7b11df4c274b39132ec5b5d1036cc3f078eddd629a3bcbfd8e7ad6d50e5e922c6e74e1051330d3ca47a6bd049033d0ef04a8db1>
2015-08-16 11:54:08 +0000

    Add WebApiExtended with basic cache controller
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS27]$/pubcomp/Caching;C31337

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -3,10 +3,10 @@ using System.Reflection;
 [assembly: AssemblyCompany("Public Components by Danny Varod")]
 [assembly: AssemblyTrademark("https://pubcomp.codeplex.com/")]
 
-[assembly: AssemblyFileVersion("1.4.1.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.4.1")]
+[assembly: AssemblyInformationalVersion("1.5.0")]
 #else
-    [assembly: AssemblyInformationalVersion("1.4.1")]
+    [assembly: AssemblyInformationalVersion("1.5.0")]
 #endif
