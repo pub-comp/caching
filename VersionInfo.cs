4430ad96d66cfef3c8447be212c8c8b51ba37e0c
Public Components <pub-comp@users.noreply.github.com>
2018-07-25 15:15:06 +0300

    Update VersionInfo.cs

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -3,10 +3,10 @@ using System.Reflection;
 [assembly: AssemblyCompany("Public Components by Danny Varod")]
 [assembly: AssemblyTrademark("https://github.com/pub-comp/caching")]
 
-[assembly: AssemblyFileVersion("3.1.4")]
+[assembly: AssemblyFileVersion("3.1.5")]
 
 #if DEBUG
-    [assembly: AssemblyInformationalVersion("3.1.4-PreRelease0")]
+    [assembly: AssemblyInformationalVersion("3.1.5-PreRelease0")]
 #else
-    [assembly: AssemblyInformationalVersion("3.1.4")]
+    [assembly: AssemblyInformationalVersion("3.1.5")]
 #endif
