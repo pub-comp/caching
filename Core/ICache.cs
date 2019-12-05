0bb4abc009a07f06d8821d25fab487ed38affd36
Avi Malka <avima@payoneer.com>
2019-12-05 12:49:30 +0200

    cleanup

--- Core/ICache.cs
+++ Core/ICache.cs
@@ -3,13 +3,6 @@ using System.Threading.Tasks;
 
 namespace PubComp.Caching.Core
 {
-    public interface ICacheWithFallback
-    {
-        bool IsActive { get; }
-
-        string FallbackCacheName { get; }
-    }
-
     public interface ICache
     {
         string Name { get; }
